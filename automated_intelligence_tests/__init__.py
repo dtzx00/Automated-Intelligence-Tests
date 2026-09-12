03bf6a864d0caf4f621c3f09a5e2b095aa238d9e
Dawei "David" Wang <35202791+dtzx00@users.noreply.github.com>
2026-09-12 13:20:52 -0500

    Bump version to 0.1.10

--- automated_intelligence_tests/__init__.py
+++ automated_intelligence_tests/__init__.py
@@ -24,7 +24,7 @@ from .cwt.parse import parse as cwt_parse
 from .cwt.evaluate import evaluate as cwt_evaluate
 
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 
 def list_available_tests() -> dict:
